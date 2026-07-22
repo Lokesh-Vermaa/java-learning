2c46d14df0341473c62d1aa550d94123eed1aee7
LOKESH VERMA <lokeshverma1426@gmail.com>
2026-07-22 11:42:27 +0530

    Solved DSA problem

--- java dsa/src/in/leetcode/arrays/ques14.java
+++ java dsa/src/in/leetcode/arrays/ques14.java
@@ -1,5 +1,63 @@
 package in.leetcode.arrays;
 
+import java.util.Scanner;
+
 public class ques14 {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int m = in.nextInt();
+        int n = in.nextInt();
+        int arraylength = in.nextInt();
+        int[][] indices = new int[arraylength][2];
+        for(int z = 0;z<arraylength;z++){
+            indices[z][0] = in.nextInt();
+            indices[z][1] = in.nextInt();
+        }
+        int result = oddCells(m, n, indices);
+        System.out.println(result);
+    }
+    public static int oddCells(int m, int n, int[][] indices) {
+        int[][] mat = new int[m][n];
+        for(int i = 0;i<indices.length;i++){
+            for(int j = 0;j<mat[indices[i][0]].length;j++){
+                mat[indices[i][0]][j]++;
+
+            }
+            for(int k = 0;k<mat.length;k++){
+                mat[k][indices[i][1]]++;
+            }
+        }
+        int count = 0;
+        for(int p = 0;p<m;p++){
+            for(int q = 0;q<n;q++){
+                if(mat[p][q]%2!=0){
+                    count++;
+                }
+                
+            }
+        }
+        return count;
+    }
 }
+//faster approach 
+/*public static int oddCells(int m, int n, int[][] indices) {
+    int[] rows = new int[m];
+    int[] cols = new int[n];
+
+    for (int[] index : indices) {
+        rows[index[0]]++;
+        cols[index[1]]++;
+    }
+
+    int count = 0;
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if ((rows[i] + cols[j]) % 2 == 1) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+} */
--- java dsa/src/in/leetcode/arrays/ques15.java
+++ java dsa/src/in/leetcode/arrays/ques15.java
@@ -1,5 +1,43 @@
 package in.leetcode.arrays;
 
+import java.util.Scanner;
+
 public class ques15 {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[][] nums = new int[n][n];
+        if(n>=1 || n<=100){
+            for(int k = 0; k<n;k++){
+            for(int l = 0;l<n;l++){
+                nums[k][l] = in.nextInt();
+
+                if (nums[k][l]<1 || nums[k][l]>100){
+                System.out.println("please enter no in range1");
+                return;
+            }
+            }
+        }
+        }else{
+            System.out.println("please enter no in range2");
+            return;
+        }
+        int result = diagonalSum(nums);
+        System.out.println(result); 
+
+    }
+    public static int diagonalSum(int[][] mat) {
+        int sum = 0;
+
+        for (int i = 0;i<mat.length;i++){
+                sum += mat[i][i] + mat[i][mat.length-i-1];
+           
+        }
+        if (mat.length%2==0){
+            return sum;
+        }else{
+            return sum-mat[mat.length/2][mat.length/2];
+        }
+        
+    }
 }
