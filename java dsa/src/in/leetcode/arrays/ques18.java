b48a0baee1f13d471022254ded61f38f2e255205
LOKESH VERMA <lokeshverma1426@gmail.com>
2026-07-23 10:02:57 +0530

    dsa problem solved

--- java dsa/src/in/leetcode/arrays/ques18.java
+++ java dsa/src/in/leetcode/arrays/ques18.java
@@ -1,5 +1,58 @@
 package in.leetcode.arrays;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class ques18 {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] nums = new int[n];
+        if(n>=1 || n<=Math.pow(10, 4)){
+            for(int k = 0; k<n;k++){
+                nums[k] = in.nextInt();
+                if (nums[0]==0){
+                    System.out.println("please enter no in range1");
+                    return;
+                }
+
+                if (nums[k]<0 || nums[k]>9){
+                System.out.println("please enter no in range1");
+                return;
+            }
+            }
+        }else{
+            System.out.println("please enter no in range2");
+            return;
+        }
+        int k = in.nextInt();
+        if (k<1 || k>Math.pow(10, 4)){
+            System.out.println("please enter no in range2");
+            return;
+        }
+        List<Integer> result = addToArrayForm(nums, k);
+        System.out.println(result); 
+    }
+    public static List<Integer> addToArrayForm(int[] num, int k) {
+    List<Integer> ans = new ArrayList<>();
+
+    int i = num.length - 1;
+
+    while (i >= 0 || k > 0) {
+
+        if (i >= 0) {
+            k += num[i];
+            i--;
+        }
+
+        ans.add(k % 10);
+        k /= 10;
+    }
+
+    Collections.reverse(ans);
+    return ans;
 }
+}
+
--- java dsa/src/in/leetcode/arrays/ques19.java
+++ java dsa/src/in/leetcode/arrays/ques19.java
@@ -1,5 +1,51 @@
 package in.leetcode.arrays;
 
 public class ques19 {
-    
+    public int maximumPopulation(int[][] logs) {
+
+    int[] population = new int[101];
+
+    for (int[] log : logs) {
+        for (int year = log[0]; year < log[1]; year++) {
+            population[year - 1950]++;
+        }
+    }
+
+    int max = 0;
+    int ans = 1950;
+
+    for (int i = 0; i < 101; i++) {
+        if (population[i] > max) {
+            max = population[i];
+            ans = i + 1950;
+        }
+    }
+
+    return ans;
+}
+//faster approach
+public int maximumPopulation1(int[][] logs) {
+
+    int[] diff = new int[101];
+
+    for (int[] log : logs) {
+        diff[log[0] - 1950]++;
+        diff[log[1] - 1950]--;
+    }
+
+    int max = 0;
+    int population = 0;
+    int ans = 1950;
+
+    for (int i = 0; i < 101; i++) {
+        population += diff[i];
+
+        if (population > max) {
+            max = population;
+            ans = i + 1950;
+        }
+    }
+
+    return ans;
+}
 }
